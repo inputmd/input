d5e9e61cf693695c04e827efd8f0c7efed7f6c8b
Raymond Z <raykyri@gmail.com>
2026-02-21 01:42:11 -0500

    feat: add radix, lucide, user menu

--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,4 +1,7 @@
 import type { GitHubUser } from '../github';
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
+import * as Popover from '@radix-ui/react-popover';
+import * as Tooltip from '@radix-ui/react-tooltip';
 
 export type ActiveView = 'auth' | 'documents' | 'githubapp' | 'repodocuments' | 'loading' | 'error' | 'content' | 'edit';
 
@@ -92,11 +95,58 @@ export function Toolbar({
           <span>{theme === 'dark' ? '\u2600' : '\u263D'}</span>
         </button>
         {user ? (
-          <div class="user-info">
-            <img class="user-avatar" src={user.avatar_url} alt="" width={24} height={24} />
-            <span class="user-name">{user.name ?? user.login}</span>
-            <button type="button" onClick={onSignOut}>Sign Out</button>
-          </div>
+          <Tooltip.Provider delayDuration={250}>
+            <DropdownMenu.Root>
+              <Tooltip.Root>
+                <DropdownMenu.Trigger asChild>
+                  <Tooltip.Trigger asChild>
+                    <button type="button" class="user-menu-trigger" aria-label="User menu">
+                      <img class="user-avatar" src={user.avatar_url} alt="" width={24} height={24} />
+                      <span class="user-name">{user.name ?? user.login}</span>
+                    </button>
+                  </Tooltip.Trigger>
+                </DropdownMenu.Trigger>
+                <Tooltip.Portal>
+                  <Tooltip.Content class="toolbar-tooltip" side="bottom" align="end" sideOffset={6}>
+                    User menu
+                    <Tooltip.Arrow class="toolbar-tooltip-arrow" />
+                  </Tooltip.Content>
+                </Tooltip.Portal>
+              </Tooltip.Root>
+              <DropdownMenu.Portal>
+                <DropdownMenu.Content class="user-menu-content" sideOffset={6} align="end">
+                  <DropdownMenu.Label class="user-menu-label">{user.login}</DropdownMenu.Label>
+                  <DropdownMenu.Separator class="user-menu-separator" />
+                  <Popover.Root>
+                    <Popover.Trigger asChild>
+                      <DropdownMenu.Item
+                        class="user-menu-item"
+                        onSelect={(e: Event) => e.preventDefault()}
+                      >
+                        Account Details
+                      </DropdownMenu.Item>
+                    </Popover.Trigger>
+                    <Popover.Portal>
+                      <Popover.Content class="user-popover-content" side="left" align="start" sideOffset={8}>
+                        <div class="user-popover-row">
+                          <img class="user-avatar" src={user.avatar_url} alt="" width={32} height={32} />
+                          <div class="user-popover-meta">
+                            <div>{user.name ?? user.login}</div>
+                            <div>@{user.login}</div>
+                          </div>
+                        </div>
+                        <Popover.Arrow class="user-popover-arrow" />
+                      </Popover.Content>
+                    </Popover.Portal>
+                  </Popover.Root>
+                  <DropdownMenu.Separator class="user-menu-separator" />
+                  <DropdownMenu.Item class="user-menu-item" onSelect={() => onSignOut()}>
+                    Sign Out
+                  </DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu.Portal>
+            </DropdownMenu.Root>
+          </Tooltip.Provider>
         ) : !showSignInToSave && view !== 'auth' ? (
           <button type="button" onClick={() => navigate('auth')}>Sign In</button>
         ) : null}
