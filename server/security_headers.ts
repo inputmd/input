ce5561efc5e6556269f8714812f601801402143f
Raymond Z <raykyri@gmail.com>
2026-02-21 12:16:39 -0500

    feat: add HSTS header to security headers

--- server/security_headers.ts
+++ server/security_headers.ts
@@ -4,5 +4,6 @@ import { CONTENT_SECURITY_POLICY } from './config';
 export function applySecurityHeaders(res: http.ServerResponse): void {
   res.setHeader('X-Content-Type-Options', 'nosniff');
   res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('Strict-Transport-Security', 'max-age=63072000; includeSubDomains; preload');
   res.setHeader('Content-Security-Policy', CONTENT_SECURITY_POLICY);
 }
